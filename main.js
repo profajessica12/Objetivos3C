cdd9f94ec9c61e1a255966c85d8f32f1280b46bf
Jéssica Nascimento <160242497+profajessica12@users.noreply.github.com>
2024-06-18 09:55:22 -0300

    horas

--- main.js
+++ main.js
@@ -8,5 +8,11 @@ for(let i=0;i <botoes.length;i++){
         }
         botoes[i].classList.add("ativo");    }}
         const contadores = document.querySelectorAll(“.contador”);
-        const tempoObjetivo1 = new Date(“2024-10-05”);
-        contadores[0].textContent = tempoObjetivo1;
+        const tempoObjetivo1 = new Date(“2024-10-05T00:00:00”);
+        contadores[0].textContent = calculaTEMPO(tempoObjetivo1);
+        function calculaTempo(tempoObjetivo) {
+            let tempoAtual = new Date();
+            let tempoFinal = tempoObjetivo - tempoAtual;
+            let segundos = tempoFinal / 1000;
+            let minutos = Math.floor(segundos / 60);
+            return horas; }
